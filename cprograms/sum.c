0add4608746d18df55580654afc2391c719ceac1
mahisagar <mahisagar44@gmail.com>
2019-01-31 11:46:23 +0530

    10

--- cprograms/sum.c
+++ cprograms/sum.c
@@ -1,12 +1,14 @@
+
 #include <stdio.h>
 
-int main(void)
+int main() 
 {
-    int NumberOne, NumberTwo, Sum;
-    printf("Enter the first number:");
-    scanf("%d", &NumberOne);
-    printf("Enter the second number:");
-    scanf("%d", &NumberTwo);
-    Sum = NumberOne + NumberTwo;
-    printf("The sum of %d and %d is %d", NumberOneSum);
+  int Number1, Number2, Sum;
+  printf("To add two numbers, enter the first number:");
+  scanf("%d", &Number1);
+  printf("Enter the second number:");
+  scanf("%d", &Number2);
+  Sum = Number1 + Number2;
+  printf("The sum of %d and %d is %d.", Number1, Number2, Sum);
+  return 0;
 }
