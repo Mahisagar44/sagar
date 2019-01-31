8dde89e2fbde486ddb63e8d451e511b30891b6b9
mahisagar <mahisagar44@gmail.com>
2019-01-31 12:28:23 +0530

    10

---
+++ javascriptprograms/sum.js
@@ -0,0 +1,4 @@
+NumberOne = window.prompt("To find sum of two numbers, Enter the first number:");
+NumberTw0 = window.prompt("Enter the second number:");
+Sum = parseInt(NumberOne) + parseInt(NumberTw0);
+window.alert("The sum of " + NumberOne + " and " + NumberTw0 + " is "+ Sum + ".");
